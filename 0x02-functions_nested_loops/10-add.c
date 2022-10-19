cedb47acff684e391819aaabbad11472a17ac75f
gekwonder <gekwonder@gmail.com>
2022-10-19 13:34:54 +0000

    all rest

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add ttoe numbers
+ * @a: take the first number
+ * @b: take the second number
+ * Return: the sum.
+ */
+ind add(int a, int b)
+{
+	return (a + b);
+}
