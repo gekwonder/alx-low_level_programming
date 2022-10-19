df23c8c291f6ce237da4f63d9cc88d593f2eb8d4
gekwonder <gekwonder@gmail.com>
2022-10-19 10:35:39 +0000

    absoltute return

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - checking for absolute integer
+ * @t: the integer computer
+ * Return: something always
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
