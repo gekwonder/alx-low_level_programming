8439ca913225c57591277456d77edc3874c8f7b3
gekwonder <gekwonder@gmail.com>
2022-10-20 22:47:09 +0000

    nay

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinity loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 *{
+	 *	_putchar(i);
+	 *}
+	 */
+	printf("Infinte loop avoided! \\o/\n");
+
+	return (0);
+}
