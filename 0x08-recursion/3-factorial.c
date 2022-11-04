177620d3897c4240edc3661c66014bd640afb6e0
gekwonder <gekwonder@gmail.com>
2022-11-04 03:32:26 +0000

    recussing task 123

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *factorial - returns the factorial
+ *@n: integer
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+		return (n * factorial(n  -  1));
+}
