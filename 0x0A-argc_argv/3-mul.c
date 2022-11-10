af50ec1351b8e6f863d33934fb21610be368d51f
gekwonder <gekwonder@gmail.com>
2022-11-10 01:31:18 +0000

    another challenge on arguments wiht main

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - A program that multiplies two numbers
+ * @argc: The arguments' counter
+ * @argv: The arguments' values
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+		return (0);
+	}
+}
