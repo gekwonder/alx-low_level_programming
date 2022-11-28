30896ef622dd591e54d87a2563f0a7931ca71c5d
gekwonder <gekwonder@gmail.com>
2022-11-28 00:26:25 +0000

    again

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,21 +2,21 @@
 #define _DOG_H_
 
 /**
-* struct dog - structure for a basic dog
-* @name: Dog's name
-* @age: Dog's age
-* @owner: Dog's owner
-* Description: data structure for dog
-*/
-typedef struct dog
+ * struct dog - structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
-void _putchar(char c);
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* struct definition */
+#endif
