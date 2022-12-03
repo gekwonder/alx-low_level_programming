e89090abb26923752eb881abfb43c8543f9f39de
gekwonder <gekwonder@gmail.com>
2022-12-03 10:30:09 +0000

    okay

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,14 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	unsigned long int max = 0x01;
+
+	max <<= index;
+	if (max == 0)
 		return (-1);
 
-	if ((n & (1 << index)) == 0)
+	if ((n & max))
 		return (1);
-
-	return (1);
+	else
+		return (0);
 }
